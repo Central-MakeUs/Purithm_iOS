9cdcb45123103df54a2956477a87adef86f8b681
mumu <tnddls2ek@naver.com>
2024-07-18 14:32:16 +0900

    [Feat] Repository와 UseCase의 로직을 분리 #3
    1. UseCase 에는 비즈니스 로직만을, Repository 에는 데이터 CRUD 로직만을 구현하기 위해 로직 분리
    2. 불필요한 AuthManager 삭제

--- Projects/Core/PurithmAuth/Sources/AuthManager.swift
+++ Projects/Core/PurithmAuth/Sources/AuthManager.swift
@@ -6,32 +6,3 @@
 //
 
 import Combine
-
-//TODO: Authorization 는 Core로 분리하자.
-
-public final class AuthManager {
-    public static let shared = AuthManager()
-    var cancellables = Set<AnyCancellable>()
-    
-    private let authRepository: AuthenticationManageable = AuthRepository()
-    
-    private init() { }
-}
-
-extension AuthManager {
-    public func isAlreadyLoggedIn() -> Bool {
-        //TODO: purithm service token check in local keychain
-        return authRepository.isAlreadyLoggedIn()
-    }
-    
-    // apple token
-    // name idToken
-    public func loginWithKakao() -> AnyPublisher<Void, Error> {
-        authRepository.loginWithKakao()
-    }
-    
-    public func loginWithApple() -> AnyPublisher<Void, Error> {
-        //TODO: idToken, name 전달해야함!!
-        authRepository.loginWithApple(with: "idToken", name: "이름 ~~~")
-    }
-}
--- Projects/Core/PurithmAuth/Sources/Repository/AuthRepository.swift
+++ Projects/Core/PurithmAuth/Sources/Repository/AuthRepository.swift
@@ -7,229 +7,26 @@
 
 import Combine
 import RxSwift
-import RxCocoa
-
-import KakaoSDKCommon
-import RxKakaoSDKCommon
-import KakaoSDKAuth
-import RxKakaoSDKAuth
-import KakaoSDKUser
-import RxKakaoSDKUser
-import AuthenticationServices
-import CoreNetwork
-
-import Alamofire
-import CombineExt
 import CoreKeychain
 
-protocol AuthenticationManageable {
-    func isAlreadyLoggedIn() -> Bool
-    func loginWithKakao() -> AnyPublisher<Void, Error>
-    func loginWithApple(with idToken: String, name: String) -> AnyPublisher<Void, Error>
-}
-
-public final class AuthRepository: AuthenticationManageable {
-    public typealias TokenInformation = (accessToken: String, refreshToken: String)
-    
+public final class AuthRepository {
     let disposeBag = DisposeBag()
     var cancellables = Set<AnyCancellable>()
-}
-
-//MARK: - Login Check
-extension AuthRepository {
-    public func isAlreadyLoggedIn() -> Bool {
-        //TODO: purithm service token check in local keychain
-        return false
-    }
-}
-
-//MARK: - Kakao Login
-extension AuthRepository {
-    public func loginWithKakao() -> AnyPublisher<Void, Error> {
-        return Future { [weak self] promise in
-            guard let self else { return }
-            
-            let isKakaoTalkLoginAvailable = UserApi.isKakaoTalkLoginAvailable()
-            
-            self.kakaoLogin(
-                using: isKakaoTalkLoginAvailable ? UserApi.shared.rx.loginWithKakaoTalk() : UserApi.shared.rx.loginWithKakaoAccount(),
-                errorType: isKakaoTalkLoginAvailable ? .KakaoTalkLoginFailed : .KakaoAccountLoginFailed
-            )
-            .flatMap { kakaoAuthentication -> Observable<PurithmTokenResponse> in
-                return self.loginToPurithmServiceForKakao(with: kakaoAuthentication)
-            }
-            .subscribe(onNext: { purithmToken in
-                // token 키체인에 저장
-                do {
-                    try KeychainManager.shared.saveAuthToken(
-                        accessToken: purithmToken.accessToken,
-                        refreshToken: purithmToken.refreshToken
-                    )
-                } catch {
-                    print("::: 키체인 저장 실패")
-                    promise(.failure(error))
-                }
-                promise(.success(Void()))
-            }, onError: { error in
-                promise(.failure(error))
-            })
-            .disposed(by: self.disposeBag)
-        }
-        .eraseToAnyPublisher()
+    
+    public init() { }
+    
+    public func saveAuthToken(accessToken: String, refreshToken: String) throws {
+        try KeychainManager.shared.saveAuthToken(
+            accessToken: accessToken,
+            refreshToken: refreshToken
+        )
     }
     
-    private func kakaoLogin(using loginObservable: Observable<OAuthToken>, errorType: AuthError) -> Observable<KakaoLoginRequest> {
-        let loginObservable = Observable<OAuthToken>.create { observer in
-            let loginObserver = loginObservable.asObservable()
-                .catch { error in
-                    return Observable.error(errorType)
-                }
-                .subscribe(onNext: { oauthToken in
-                    print("::: oauthToken success")
-                    observer.onNext(oauthToken)
-                    observer.onCompleted()
-                }, onError: { error in
-                    print("::: oauthToken error > \(error as? AuthError)")
-                    observer.onError(error)
-                })
-            
-            return Disposables.create {
-                loginObserver.dispose()
-            }
-        }
-        
-        let userObsevable = Observable<User>.create { observer in
-            let userObserver = UserApi.shared.rx.me().asObservable()
-                .catch { error in
-                    return Observable.error(errorType)
-                }
-                .subscribe(onNext: { user in
-                    print("::: user success")
-                    observer.onNext(user)
-                    observer.onCompleted()
-                }, onError: { error in
-                    print("::: user error > \(error as? AuthError)")
-                    observer.onError(error)
-                })
-            
-            return Disposables.create {
-                userObserver.dispose()
-            }
-        }
-        
-        return loginObservable
-            .flatMap { oauthToken in
-                userObsevable.map { user in
-                    (oauthToken, user)
-                }
-            }
-            .map { (oauthToken, user) in
-                
-                let response = KakaoLoginRequest(
-                    userName: user.kakaoAccount?.profile?.nickname ?? "",
-                    accessToken: oauthToken.accessToken,
-                    refreshToken: oauthToken.refreshToken
-                )
-                print("::: kakao login response")
-                return response
-            }
-            .asObservable()
+    public func retriveAuthToken() throws -> PurithmAuthToken {
+        try KeychainManager.shared.retriveAuthToken()
     }
     
-    // TODO: 실제 카카오 토큰 정보를 Purithm 서버로 넘기기 + 서버 response interface 맞춰서 response type 설정하기
-    private func loginToPurithmServiceForKakao(with parameter: KakaoLoginRequest) -> Observable<PurithmTokenResponse> {
-        //TODO: Return 타입 정의 필요 + 해당 위치에서 받아온 값 Keychain 에 저장
-        let mockJson = """
-                        {
-                            "code": 200,
-                            "timestamp": "timestamp",
-                            "data": {
-                                "name": "name",
-                                "accessToken": "accessToken",
-                                "refreshToken": "refreshToken"
-                            },
-                            "message": "message"
-                        }
-                        """
-        
-        return KakaoLoginRequestBuilder().mockRequest(from: mockJson).asObservable()
+    public func deleteAuthToken() throws {
+        try KeychainManager.shared.deleteAuthToken()
     }
 }
-
-//MARK: - Apple Login
-extension AuthRepository {
-    public func loginWithApple(with idToken: String, name: String) -> AnyPublisher<Void, Error> {
-        let appleAuth = AppleLoginRequest(userName: name, idToken: idToken)
-        
-        return Future { [weak self] promise in
-            guard let self else {
-                promise(.failure(AuthError.referenceInvalidError))
-                return
-            }
-            let publisher = loginToPurithmServiceForApple(with: appleAuth).share().materialize()
-                
-            publisher.values()
-                .sink { response in
-                    // token 키체인에 저장
-                    try? KeychainManager.shared.saveAuthToken(
-                        accessToken: response.accessToken,
-                        refreshToken: response.refreshToken
-                    )
-                    promise(.success(Void()))
-                }
-                .store(in: &cancellables)
-            
-            publisher.failures()
-                .sink { error in
-                    promise(.failure(error))
-                }
-                .store(in: &cancellables)
-        }
-        .eraseToAnyPublisher()
-    }
-    
-    private func loginToPurithmServiceForApple(with parameter: AppleLoginRequest) -> AnyPublisher<PurithmTokenResponse, Error> {
-        let mockJson = """
-                        {
-                            "code": 200,
-                            "timestamp": "timestamp",
-                            "data": {
-                                "name": "name",
-                                "accessToken": "accessToken",
-                                "refreshToken": "refreshToken"
-                            },
-                            "message": "message"
-                        }
-                        """
-        
-        return AppleLoginRequestBuilder().mockRequest(from: mockJson)
-    }
-}
-
-//TODO: Core로 옮기자
-extension Publisher {
-    func asObservable() -> Observable<Output> {
-        Observable<Output>.create { observer in
-            let cancellable = self.sink { completion in
-                switch completion {
-                case .finished:
-                    observer.onCompleted()
-                case .failure(let error):
-                    observer.onError(error)
-                }
-            } receiveValue: { value in
-                observer.onNext(value)
-            }
-            
-            return Disposables.create {
-                cancellable.cancel()
-            }
-        }
-    }
-}
-
-//to-do list
-// 1. 네트워크 모듈 추가하기 [x]
-// 2. mocking을 포함한 서비스 통신로직 구현 [x]
-// 3. auth feature 모듈을 상세 분리하기. auth는 core로, login은 feature로 추가하자
-

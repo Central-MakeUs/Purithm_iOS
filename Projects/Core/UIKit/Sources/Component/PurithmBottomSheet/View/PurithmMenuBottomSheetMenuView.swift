b5abb96237094e1a84ef3c1824ce5e819ba00e1b
SoongInLee <tnddls2ek@naver.com>
2024-08-07 12:44:05 +0900

    [Feat] 메뉴 바텀시트 UI 작업 및 이벤트 바인딩 작업 완료

---
+++ Projects/Core/UIKit/Sources/Component/PurithmBottomSheet/View/PurithmMenuBottomSheetMenuView.swift
@@ -0,0 +1,49 @@
+//
+//  PurithmMenuBottomSheetMenuView.swift
+//  CoreUIKit
+//
+//  Created by 이숭인 on 8/7/24.
+//
+
+import UIKit
+
+final class PurithmMenuBottomSheetMenuView: BaseView {
+    let container = UIView().then {
+        $0.isUserInteractionEnabled = true
+    }
+    let label = PurithmLabel(typography: Constants.labelTypo).then {
+        $0.isUserInteractionEnabled = false
+    }
+    let labelTapGesture = UITapGestureRecognizer()
+    
+    override func setupSubviews() {
+        addSubview(container)
+        
+        container.addSubview(label)
+        container.addGestureRecognizer(labelTapGesture)
+    }
+    
+    override func setupConstraints() {
+        container.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+            make.height.equalTo(58)
+        }
+        
+        label.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.horizontalEdges.equalToSuperview().inset(20)
+            make.verticalEdges.lessThanOrEqualToSuperview()
+        }
+    }
+    
+    func configure(with menu: PurithmMenuModel) {
+        label.text = menu.title
+        label.textColor = menu.isSelected ? .gray500 : .gray300
+    }
+}
+
+extension PurithmMenuBottomSheetMenuView {
+    private enum Constants {
+        static let labelTypo = Typography(size: .size18, weight: .semibold, color: .gray500, applyLineHeight: true)
+    }
+}
---
+++ Projects/Core/UIKit/Sources/Component/PurithmBottomSheet/View/PurithmMenuBottomSheetView.swift
@@ -0,0 +1,57 @@
+//
+//  PurithmMenuBottomSheetView.swift
+//  CoreUIKit
+//
+//  Created by 이숭인 on 8/6/24.
+//
+
+import UIKit
+import SnapKit
+import Then
+import Combine
+import CoreCommonKit
+
+public final class PurithmMenuBottomSheetView: BaseView {
+    private var cancellables = Set<AnyCancellable>()
+    let container = UIStackView().then {
+        $0.axis = .vertical
+        $0.distribution = .fillProportionally
+        $0.alignment = .fill
+        $0.backgroundColor = .gray100
+    }
+    private let tapEventSubject = PassthroughSubject<String, Never>()
+    var menuTapEvent: AnyPublisher<String, Never> {
+        tapEventSubject.eraseToAnyPublisher()
+    }
+    
+    public override func setupSubviews() {
+        addSubview(container)
+    }
+    
+    public override func setupConstraints() {
+        container.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview()
+            make.top.equalToSuperview().inset(20)
+            make.bottom.equalTo(safeAreaLayoutGuide)
+        }
+    }
+    
+    func configure(with menus: [PurithmMenuModel]) {
+        let menuList: [PurithmMenuBottomSheetMenuView] = menus.map { menu in
+            let menuView = PurithmMenuBottomSheetMenuView()
+            menuView.configure(with: menu)
+            menuView.labelTapGesture.tapPublisher
+                .sink { [weak self] _ in
+                    self?.tapEventSubject.send(menu.identifier)
+                }
+                .store(in: &cancellables)
+            return menuView
+        }
+        
+        menuList.forEach {
+            container.addArrangedSubview($0)
+        }
+        
+        container.layoutIfNeeded()
+    }
+}

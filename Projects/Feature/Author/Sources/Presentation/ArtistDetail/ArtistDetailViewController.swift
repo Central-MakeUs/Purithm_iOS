a4ad2112fa819e15cdf7fd81e09b18de0575b304
SoongInLee <tnddls2ek@naver.com>
2024-08-09 01:57:32 +0900

    [Feat] 작가 상세화면 네비게이션 적용

--- Projects/Feature/Author/Sources/Presentation/ArtistDetail/ArtistDetailViewController.swift
+++ Projects/Feature/Author/Sources/Presentation/ArtistDetail/ArtistDetailViewController.swift
@@ -21,4 +21,22 @@ final class ArtistDetailViewController: ViewController<ArtistDetailView> {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        initNavigationBar(with: .page)
+    }
+}
+
+extension ArtistDetailViewController: NavigationBarApplicable {
+    var leftButtonItems: [NavigationBarButtonItemType] {
+        return [
+            .backImage(
+                identifier: "left_back_bar_button",
+                image: .icArrowLeft,
+                color: .gray500
+            )
+        ]
+    }
 }

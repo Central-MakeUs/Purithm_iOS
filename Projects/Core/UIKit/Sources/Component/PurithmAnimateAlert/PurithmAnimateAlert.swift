167e6acd675af29a0c8ad454e05874501546fa8a
SoongInLee <tnddls2ek@naver.com>
2024-08-12 01:11:33 +0900

    [Feat] 스탬프 적립 완료 알럿 화면 UI 구현

---
+++ Projects/Core/UIKit/Sources/Component/PurithmAnimateAlert/PurithmAnimateAlert.swift
@@ -0,0 +1,30 @@
+//
+//  PurithmAnimateAlert.swift
+//  CoreUIKit
+//
+//  Created by 이숭인 on 8/11/24.
+//
+
+import UIKit
+import Combine
+
+public final class PurithmAnimateAlert<Content: BaseView>: ViewController<Content> {
+    private var cancellables = Set<AnyCancellable>()
+    
+    public override init() {
+        super.init()
+        
+    }
+    
+    public required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+            self.dismiss(animated: false)
+        }
+    }
+}

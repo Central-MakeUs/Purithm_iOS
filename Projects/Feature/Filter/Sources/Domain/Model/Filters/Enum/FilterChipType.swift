dd9e60ebadd9ac2d0400b86970ecb4528905de1b
mumu <tnddls2ek@naver.com>
2024-08-15 14:14:15 +0900

    [Update] 필터 칩 설정값 변경 및 tag값 정의

--- Projects/Feature/Filter/Sources/Domain/Model/Filters/Enum/FilterChipType.swift
+++ Projects/Feature/Filter/Sources/Domain/Model/Filters/Enum/FilterChipType.swift
@@ -8,16 +8,41 @@
 import Foundation
 
 public enum FilterChipType: String, CaseIterable {
-    case spring = "봄"
-    case summer = "여름"
-    case autumn = "가을"
-    case winter = "겨울"
+    case all = "전체"
+    case brightSpringDay = "화사한 봄날"
+    case summerLight = "여름빛"
+    case autumnScenery = "가을 풍경"
+    case winterSnow = "겨울눈"
     case backlight = "역광에서"
     case night = "night"
     case daily = "daily"
+    case cat = "고양이"
     
     var title: String {
         return self.rawValue
+    }
+    
+    var tag: String {
+        switch self {
+        case .all:
+            return ""
+        case .brightSpringDay:
+            return "spring"
+        case .summerLight:
+            return "summer"
+        case .autumnScenery:
+            return "fall"
+        case .winterSnow:
+            return "winter"
+        case .backlight:
+            return "backlight"
+        case .night:
+            return "night"
+        case .daily:
+            return "daily"
+        case .cat:
+            return "cat"
+        }
     }
     
     var identifier: String {

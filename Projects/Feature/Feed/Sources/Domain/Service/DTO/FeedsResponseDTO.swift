fbe90f87a112e363b78d02e06c921f972969caee
mumu <tnddls2ek@naver.com>
2024-08-19 17:39:57 +0900

    [update] 서버 응답 변경사항 반영
    - profile 변수를 nilable하게 수정

--- Projects/Feature/Feed/Sources/Domain/Service/DTO/FeedsResponseDTO.swift
+++ Projects/Feature/Feed/Sources/Domain/Service/DTO/FeedsResponseDTO.swift
@@ -21,6 +21,24 @@ public struct FeedsResponseDTO: Codable {
     let id: Int
     let filterThumbnail: String
     
+    public init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.filterId = try container.decode(Int.self, forKey: .filterId)
+        self.filterName = try container.decode(String.self, forKey: .filterName)
+        self.writer = try container.decode(String.self, forKey: .writer)
+        self.profile = try container.decodeIfPresent(String.self, forKey: .profile) ?? ""
+        self.pureDegree = try container.decode(Int.self, forKey: .pureDegree)
+        self.content = try container.decode(String.self, forKey: .content)
+        self.createdAt = try container.decode(String.self, forKey: .createdAt)
+        self.pictures = try container.decode([String].self, forKey: .pictures)
+        self.id = try container.decode(Int.self, forKey: .id)
+        self.filterThumbnail = try container.decode(String.self, forKey: .filterThumbnail)
+    }
+    
+    func retriveFilterInformation() -> (name: String, thumbnail: String) {
+        return (filterName, filterThumbnail)
+    }
+    
     func convertModel() -> FeedReviewModel {
         FeedReviewModel(
             identifier: "\(filterId)",

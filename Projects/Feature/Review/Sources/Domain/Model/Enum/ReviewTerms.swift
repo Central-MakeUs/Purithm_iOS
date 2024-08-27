151e97bc6bccd11469f836edee2b118f994060e8
mumu <tnddls2ek@naver.com>
2024-08-27 20:16:32 +0900

    [update] 약관 항목 추가

--- Projects/Feature/Review/Sources/Domain/Model/Enum/ReviewTerms.swift
+++ Projects/Feature/Review/Sources/Domain/Model/Enum/ReviewTerms.swift
@@ -8,10 +8,13 @@
 import Foundation
 
 enum ReviewTerms: CaseIterable {
+    case agree
     case promotionalContentConsent
     
     var title: String {
         switch self {
+        case .agree:
+            return "(필수) 후기 약관에 동의합니다."
         case .promotionalContentConsent:
             return "(필수) 후기는 홍보 컨텐츠로 사용될 수 있습니다."
         }
